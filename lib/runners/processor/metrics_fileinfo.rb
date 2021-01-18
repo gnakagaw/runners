12b6d40b97a87103b5d50b2abfac2c48ed8c8dee
Gaku Nakagawa <gaku@sleeek.io>
2021-01-18 16:34:36 +0900

    support multi-byte char files

--- lib/runners/processor/metrics_fileinfo.rb
+++ lib/runners/processor/metrics_fileinfo.rb
@@ -3,10 +3,13 @@ module Runners
     Schema = _ = StrongJSON.new do
       let :runner_config, Schema::BaseConfig.base
       let :issue, object(
-          line_of_code: integer,
-          last_commit_datetime: string?
+          line_of_code: integer?,
+          last_commit_datetime: string
       )
     end
+
+    FILE_COMMAND_OUTPUT_PATTERN = /^(.+?:) (.*)/.freeze
+    FILE_COMMAND_VALID_PLAIN_TEXT_FORMATS = ["ASCII text", "UTF-8 Unicode text", "ISO-8859 text"].freeze
 
     def analyzer_version
       stdout, _, _ = capture3!("wc", "--version")
@@ -37,7 +40,7 @@ module Runners
                 path: relative_path(file),
                 location: nil,
                 id: "metrics_fileinfo",
-                message: "hello.rb: loc = #{loc}, last commit datetime = #{commit_date}",
+                message: "#{file}: loc = #{loc}, last commit datetime = #{commit_date}",
                 object: {
                     line_of_code: loc,
                     last_commit_datetime: commit_date
@@ -67,7 +70,10 @@ module Runners
     end
 
     def is_text_file?(target_file)
-      true
+      stdout, _, _ = capture3!("file", "-E", target_file)
+      FILE_COMMAND_OUTPUT_PATTERN.match(stdout).to_a[2].split(",").any? do |type|
+        FILE_COMMAND_VALID_PLAIN_TEXT_FORMATS.any? { |t| type.include?(t) }
+      end
     end
 
     def analyze_last_commit_date(target_files)
