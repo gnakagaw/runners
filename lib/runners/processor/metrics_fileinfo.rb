fda89f5c2c1be2b7775e82bd2f9afcfc90a5d995
Gaku Nakagawa <gaku@sleeek.io>
2021-01-20 12:08:16 +0900

    refactoring: change analysis path

--- lib/runners/processor/metrics_fileinfo.rb
+++ lib/runners/processor/metrics_fileinfo.rb
@@ -22,43 +22,43 @@ module Runners
     def analyze(changes)
       Results::Success.new(guid: guid, analyzer: analyzer).tap do |result|
         changes.changed_paths.each do |path|
-          result.add_issue(generate_issue(path, analyze_line_of_code(path), analyze_last_commit_datetime(path)))
+          result.add_issue(generate_issue(path))
         end
       end
     end
 
     private
 
-    def generate_issue(path, loc, commit_datetime)
+    def generate_issue(path)
+      filepath = relative_path(path).to_s
+      loc = analyze_line_of_code(filepath)
+      last_commit = analyze_last_commit_datetime(filepath)
+
       Issue.new(
         path: relative_path(path),
         location: nil,
         id: "metrics_fileinfo",
-        message: "#{path_str(path)}: loc = #{loc}, last commit datetime = #{commit_datetime}",
+        message: "#{filepath}: loc = #{loc}, last commit datetime = #{last_commit}",
         object: {
           line_of_code: loc,
-          last_commit_datetime: commit_datetime
+          last_commit_datetime: last_commit
         },
         schema: Schema.issue
       )
     end
 
-    def analyze_line_of_code(path)
-      is_text_file?(path) ? capture3!("wc", "-l", path_str(path)).then {|stdout,| Integer(stdout.split(" ")[0])} : nil
+    def analyze_line_of_code(target)
+      is_text_file?(target) ? capture3!("wc", "-l", target).then {|stdout,| Integer(stdout.split(" ")[0])} : nil
     end
 
-    def analyze_last_commit_datetime(path)
-      capture3!("git", "log", "-1", "--format=format:%aI", path_str(path)).then {|stdout,| stdout}
+    def analyze_last_commit_datetime(target)
+      capture3!("git", "log", "-1", "--format=format:%aI", target).then {|stdout,| stdout}
     end
 
-    def is_text_file?(path)
-      capture3!("git", "ls-files", "--eol", "--error-unmatch", path_str(path)).then do |stdout, _, _|
+    def is_text_file?(target)
+      capture3!("git", "ls-files", "--eol", "--error-unmatch", target).then do |stdout, _, _|
         stdout.split(" ")[1].match?("w/-text") ? false : true
       end
-    end
-
-    def path_str(path)
-      relative_path(path).to_s
     end
   end
 end
